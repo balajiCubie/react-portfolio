58b0d6533e9db7404e7dc7052c9ccd656940ebbc
Balaji K <balajibala76394@gmail.com>
2024-07-17 18:23:18 +0530

    Update recent projects with new assets and detailed descriptions

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -13,6 +13,7 @@ import Veritru from '../../assets/recentprojects/veritru.png';
 import Lofo from '../../assets/recentprojects/lofo.png';
 import Startup from '../../assets/recentprojects/startup.png';
 import Lacalle from '../../assets/recentprojects/lacalle.png';
+import Medical from '../../assets/recentprojects/medical.png';
 
 const useStyles = makeStyles((theme) => ({
   main: {
@@ -34,27 +35,34 @@ export const Works = () => {
     },
     {
       id: 2,
+      title: 'Healthcare-Data-Analysis',
+      description: `Analyzed the prevalence of various diseases and their distribution across different demographic groups (age, gender, location). Investigate how different demographic groups utilize healthcare services under the NTR Vaidya Seva scheme.`,
+      alter: 'Healthcare-Data-Analysis',
+      image: `${Medical}`,
+    },
+    {
+      id: 3,
       title: 'Corporation Product Sales',
       description: `I’ve recently developed a data dashboard using SN Corporation’s dataset, providing deep insights into various facets of the business. From tracking customer acquisition and sales performance to analyzing customer demographics and operational efficiency, this dashboard highlights key trends and opportunities.`,
       alter: 'Corporation Product Sales',
       image: `${Veritru}`,
     },
     {
-      id: 3,
+      id: 4,
       title: 'Alzheimer’s Disease',
       description: `Developing an Integrated Framework for Comprehensive Alzheimer's Disease Analysis. Alzheimer's disease (AD) is still a major public health concern, and understanding its course and facilitating early identification require sophisticated approaches.`,
       alter: 'Alzheimer’s Disease',
       image: `${Lofo}`,
     },
     {
-      id: 4,
+      id: 5,
       title: 'Corporate Stability Through AI',
       description: `In the thriving landscape of the American Stock Market, this investigation leverages the comprehensive US Company Bankruptcy Prediction Dataset, spanning the years 1999 to 2018, featuring critical financial variables. Moreover, this research reveals the unerring prediction of corporate bankruptcy is a paramount factor among investors, stakeholders, and corporate stability.`,
       alter: 'Corporate Stability Through AI',
       image: `${Startup}`,
     },
     {
-      id: 5,
+      id: 6,
       title: 'Machine Learning and Deep Learning',
       description: `This project aims to explore, implement, and compare various machine learning models on a
 classification dataset. This project will help you to understand the nuances of different
